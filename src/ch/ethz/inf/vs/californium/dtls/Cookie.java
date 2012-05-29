95e29b28f1e0f429a28a2c21a94c46320b2bdc9f
Stefan Jucker <stefan.jucker@gmail.com>
2012-05-29 14:13:43 +0200

    deleted unused method, added documentation and TODOs

--- src/ch/ethz/inf/vs/californium/dtls/Cookie.java
+++ src/ch/ethz/inf/vs/californium/dtls/Cookie.java
@@ -1,11 +1,26 @@
 package ch.ethz.inf.vs.californium.dtls;
 
+/**
+ * Represents a stateless cookie which is used in the {@link HelloVerifyRequest}
+ * in the DTLS handshake to prevent denial-of-service attacks. See <a
+ * href="http://tools.ietf.org/html/rfc6347#section-4.3.2">RFC 6347, 4.3.2.
+ * Handshake Protocol</a> for further details.
+ * 
+ * @author Stefan Jucker
+ * 
+ */
 public class Cookie {
+
+	/** The cookie as byte array. */
 	private byte[] cookie;
 
+	/**
+	 * Used by client, when sending a {@link ClientHello} for the first time
+	 * (empty cookie).
+	 */
 	public Cookie() {
 		/*
-		 * The DTLS server SHOULD generate cookies in such a way that they can
+		 * TODO The DTLS server SHOULD generate cookies in such a way that they can
 		 * be verified without retaining any per-client state on the server. One
 		 * technique is to have a randomly generated secret and generate cookies
 		 * as:
@@ -15,20 +30,21 @@ public class Cookie {
 		this.cookie = new byte[0];
 	}
 
+	/**
+	 * Called when sending a {@link HelloVerifyRequest} (server) or
+	 * {@link ClientHello} (client) for the second time.
+	 * 
+	 * @param cookie
+	 *            the Cookie.
+	 */
 	public Cookie(byte[] cookie) {
 		this.cookie = cookie;
 	}
 
-	public boolean isValidCookie() {
-		/*
-		 * When the second ClientHello is received, the server can verify that
-		 * the Cookie is valid and that the client can receive packets at the
-		 * given IP address.
-		 */
-
-		return false;
-	}
-	
+	/**
+	 * 
+	 * @return the number of bytes of the cookie.
+	 */
 	public int length() {
 		return cookie.length;
 	}
@@ -36,7 +52,7 @@ public class Cookie {
 	public byte[] getCookie() {
 		return cookie;
 	}
-	
+
 	public void setCookie(byte[] cookie) {
 		this.cookie = cookie;
 	}
