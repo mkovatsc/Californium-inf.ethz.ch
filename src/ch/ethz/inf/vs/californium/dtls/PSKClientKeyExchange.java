ee082d5af4507b17c712c884c0ec4199ad3812a9
Stefan Jucker <stefan.jucker@gmail.com>
2012-07-18 10:56:17 +0200

    added documentation

--- src/ch/ethz/inf/vs/californium/dtls/PSKClientKeyExchange.java
+++ src/ch/ethz/inf/vs/californium/dtls/PSKClientKeyExchange.java
@@ -1,3 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Institute for Pervasive Computing, ETH Zurich.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ * This file is part of the Californium (Cf) CoAP framework.
+ ******************************************************************************/
 package ch.ethz.inf.vs.californium.dtls;
 
 import java.io.UnsupportedEncodingException;
@@ -5,6 +35,16 @@ import java.io.UnsupportedEncodingException;
 import ch.ethz.inf.vs.californium.util.DatagramReader;
 import ch.ethz.inf.vs.californium.util.DatagramWriter;
 
+/**
+ * When using preshared keys for key agreement, the client indicates which key
+ * to use by including a "PSK identity" in this message. The server can
+ * potentially provide a "PSK identity hint" to help the client in selecting
+ * which identity to use. See <a
+ * href="http://tools.ietf.org/html/rfc4279#section-2">RFC 4279</a> for details.
+ * 
+ * @author Stefan Jucker
+ * 
+ */
 public class PSKClientKeyExchange extends ClientKeyExchange {
 
 	// DTLS-specific constants ////////////////////////////////////////
