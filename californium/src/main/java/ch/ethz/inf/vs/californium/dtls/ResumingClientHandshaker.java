32c2a6d5c0f792d524fa1cf31fdcac3f03d70078
Stefan Jucker <stefan.jucker@gmail.com>
2012-08-09 11:05:59 +0200

    added exception handling and documentation for resuming handshakers

--- californium/src/main/java/ch/ethz/inf/vs/californium/dtls/ResumingClientHandshaker.java
+++ californium/src/main/java/ch/ethz/inf/vs/californium/dtls/ResumingClientHandshaker.java
@@ -1,3 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Institute for Pervasive Computing, ETH Zurich.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ * This file is part of the Californium (Cf) CoAP framework.
+ ******************************************************************************/
 package ch.ethz.inf.vs.californium.dtls;
 
 import java.security.MessageDigest;
@@ -5,12 +35,28 @@ import java.security.SecureRandom;
 
 import ch.ethz.inf.vs.californium.coap.EndpointAddress;
 import ch.ethz.inf.vs.californium.coap.Message;
+import ch.ethz.inf.vs.californium.dtls.AlertMessage.AlertDescription;
+import ch.ethz.inf.vs.californium.dtls.AlertMessage.AlertLevel;
 
+/**
+ * The resuming client handshaker executes a abbreviated handshake by adding a
+ * valid session identifier into its ClientHello message. The message flow is
+ * depicted in <a href="http://tools.ietf.org/html/rfc5246#section-7.3">Figure
+ * 2</a>. The new keys will be generated from the master secret established from a
+ * previous full handshake.
+ * 
+ * @author Stefan Jucker
+ * 
+ */
 public class ResumingClientHandshaker extends ClientHandshaker {
+	
+	// Constructor ////////////////////////////////////////////////////
 
 	public ResumingClientHandshaker(EndpointAddress endpointAddress, Message message, DTLSSession session) {
 		super(endpointAddress, message, session);
 	}
+	
+	// Methods ////////////////////////////////////////////////////////
 
 	@Override
 	public synchronized DTLSFlight processMessage(Record record) throws HandshakeException {
@@ -53,14 +99,14 @@ public class ResumingClientHandshaker extends ClientHandshaker {
 				break;
 
 			default:
-				LOG.severe("Client received not supported resuming handshake message:\n" + fragment.toString());
-				break;
+				AlertMessage alert = new AlertMessage(AlertLevel.FATAL, AlertDescription.HANDSHAKE_FAILURE);
+				throw new HandshakeException("Client received not supported resuming handshake message:\n" + fragment.toString(), alert);
 			}
 			break;
 
 		default:
-			LOG.severe("Client received not supported record:\n" + record.toString());
-			break;
+			AlertMessage alert = new AlertMessage(AlertLevel.FATAL, AlertDescription.HANDSHAKE_FAILURE);
+			throw new HandshakeException("Client received not supported record:\n" + record.toString(), alert);
 		}
 		if (flight == null) {
 			Record nextMessage = null;
@@ -75,9 +121,7 @@ public class ResumingClientHandshaker extends ClientHandshaker {
 				flight = processMessage(nextMessage);
 			}
 		}
-
-		LOG.info("DTLS Message processed.");
-		System.out.println(record.toString());
+		LOG.info("DTLS Message processed (" + endpointAddress.toString() + "):\n" + record.toString());
 		return flight;
 	}
 
@@ -88,9 +132,9 @@ public class ResumingClientHandshaker extends ClientHandshaker {
 	 * 
 	 * @param message
 	 *            the server's finished message.
-	 * @return the last flight of the short handshake (or a Alert if the
-	 *         server's finished message can not be verified).
-	 * @throws HandshakeException 
+	 * @return the last flight of the short handshake.
+	 * @throws HandshakeException
+	 *             if the server's Finished message could not be verified.
 	 */
 	private DTLSFlight receivedServerFinished(Finished message) throws HandshakeException {
 		if (lastFlight != null) {
--- californium/src/main/java/ch/ethz/inf/vs/californium/dtls/ResumingServerHandshaker.java
+++ californium/src/main/java/ch/ethz/inf/vs/californium/dtls/ResumingServerHandshaker.java
@@ -1,18 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2012, Institute for Pervasive Computing, ETH Zurich.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ * This file is part of the Californium (Cf) CoAP framework.
+ ******************************************************************************/
 package ch.ethz.inf.vs.californium.dtls;
 
 import java.security.MessageDigest;
 import java.security.SecureRandom;
 
 import ch.ethz.inf.vs.californium.coap.EndpointAddress;
+import ch.ethz.inf.vs.californium.dtls.AlertMessage.AlertDescription;
+import ch.ethz.inf.vs.californium.dtls.AlertMessage.AlertLevel;
 
+/**
+ * The resuming server handshaker executes an abbreviated handshake when
+ * receiving a ClientHello with a set session identifier. It checks whether such
+ * a session still exists and if so, generates the new keys from the previously
+ * established master secret. The message flow is depicted in <a
+ * href="http://tools.ietf.org/html/rfc5246#section-7.3">Figure 2</a>.
+ * 
+ * @author Stefan Jucker
+ * 
+ */
 public class ResumingServerHandshaker extends ServerHandshaker {
 	
-	byte[] handshakeHash;
+	// Members ////////////////////////////////////////////////////////
+	
+	/** The handshake hash used in the Finished messages. */
+	private byte[] handshakeHash;
+	
+	// Constructor ////////////////////////////////////////////////////
 
 	public ResumingServerHandshaker(EndpointAddress endpointAddress, DTLSSession session) {
 		super(endpointAddress, session);
 		setSessionToResume(session);
 	}
+	
+	// Methods ////////////////////////////////////////////////////////
 	
 	/**
 	 * Resets the state of a session, such that it can be used to resume it.
@@ -57,15 +106,15 @@ public class ResumingServerHandshaker extends ServerHandshaker {
 				break;
 
 			default:
-				LOG.severe("Server received not supported resuming handshake message:\n" + fragment.toString());
-				break;
+				AlertMessage alert = new AlertMessage(AlertLevel.FATAL, AlertDescription.HANDSHAKE_FAILURE);
+				throw new HandshakeException("Server received not supported resuming handshake message:\n" + fragment.toString(), alert);
 			}
 
 			break;
 
 		default:
-			LOG.severe("Server received not supported record:\n" + record.toString());
-			break;
+			AlertMessage alert = new AlertMessage(AlertLevel.FATAL, AlertDescription.HANDSHAKE_FAILURE);
+			throw new HandshakeException("Server received not supported record:\n" + record.toString(), alert);
 		}
 		if (flight == null) {
 			Record nextMessage = null;
@@ -80,12 +129,21 @@ public class ResumingServerHandshaker extends ServerHandshaker {
 				flight = processMessage(nextMessage);
 			}
 		}
-		LOG.info("DTLS Message processed.");
-		System.out.println(record.toString());
+		LOG.info("DTLS Message processed (" + endpointAddress.toString() + "):\n" + record.toString());
 		return flight;
 	}
 	
+	/**
+	 * The server generates new keys from the old master secret and sends
+	 * ChangeCipherSpec and Finished message. The ClientHello contains a fresh
+	 * random value which will be needed to generate the new keys.
+	 * 
+	 * @param message
+	 *            the client's hello message.
+	 * @return the server's last flight.
+	 */
 	private DTLSFlight receivedClientHello(ClientHello message) {
+
 		DTLSFlight flight = new DTLSFlight();
 		clientHello = message;
 		
@@ -125,10 +183,19 @@ public class ResumingServerHandshaker extends ServerHandshaker {
 		return flight;
 	}
 	
+	/**
+	 * Verifies the client's Finished message. If valid, encrypted application
+	 * data can be sent, otherwise an Alert must be sent.
+	 * 
+	 * @param message
+	 *            the client's Finished message.
+	 * @throws HandshakeException
+	 *             if the client's Finished message can not be verified.
+	 */
 	private void receivedClientFinished(Finished message) throws HandshakeException {
 
 		clientFinished = message;
-		
+
 		message.verifyData(getMasterSecret(), false, handshakeHash);
 	}
 
