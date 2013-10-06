c0026dd919871eef03b8b8c0e82eda125f549898
Martin Lanter <lanterm@student.ethz.ch>
2013-10-06 17:42:24 +0200

    Added project cf-secure for secure DTLS server

---
+++ cf-secure/src/main/java/ch/ethz/inf/vs/californium/examples/SecureServer.java
@@ -0,0 +1,27 @@
+package ch.ethz.inf.vs.californium.examples;
+
+import java.net.InetSocketAddress;
+
+import ch.ethz.inf.vs.californium.network.Endpoint;
+import ch.ethz.inf.vs.californium.network.NetworkConfig;
+import ch.ethz.inf.vs.californium.server.Server;
+import ch.ethz.inf.vs.scandium.DTLSConnector;
+
+public class SecureServer {
+	
+	public static final int DTLS_PORT = 5684; // TBD
+
+	public static void main(String[] args) {
+		System.out.println("Start secure server and bind to port "+DTLS_PORT);
+		InetSocketAddress address = new InetSocketAddress(DTLS_PORT);
+		
+		Server server = new Server();
+		server.addEndpoint(new Endpoint(
+				new DTLSConnector(address), address, NetworkConfig.getStandard()));
+		server.start();
+		
+		System.out.println("Secure CoAP server is listening on port "+DTLS_PORT);
+		System.out.println("(Note that this feature is not completely implemented yet)");
+	}
+
+}
