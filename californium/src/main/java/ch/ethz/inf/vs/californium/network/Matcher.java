aaedeba4b7969069eded6f80f2d9446741ab48aa
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2013-11-07 01:09:58 -0800

    Fixed RST/ACK mix-up (Block2 random access still TODO).

--- californium/src/main/java/ch/ethz/inf/vs/californium/network/Matcher.java
+++ californium/src/main/java/ch/ethz/inf/vs/californium/network/Matcher.java
@@ -221,7 +221,7 @@ public class Matcher {
 				}
 				
 				// This request fits no exchange
-				EmptyMessage rst = EmptyMessage.newACK(request);
+				EmptyMessage rst = EmptyMessage.newRST(request);
 				forwarder.sendEmptyMessage(null, rst);
 				// ignore request
 				return null;
