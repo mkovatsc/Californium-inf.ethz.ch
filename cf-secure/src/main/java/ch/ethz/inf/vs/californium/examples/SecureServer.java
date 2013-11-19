9409f5a1827895436310c31f685a98fb9c0ce15d
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2013-11-18 20:15:45 -0800

    cf-secure matches CoAP#3 Plugtest spec.

--- cf-secure/src/main/java/ch/ethz/inf/vs/californium/examples/SecureServer.java
+++ cf-secure/src/main/java/ch/ethz/inf/vs/californium/examples/SecureServer.java
@@ -1,38 +1,41 @@
 package ch.ethz.inf.vs.californium.examples;
 
 import java.net.InetSocketAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import ch.ethz.inf.vs.californium.CalifonriumLogger;
 import ch.ethz.inf.vs.californium.coap.CoAP.ResponseCode;
-import ch.ethz.inf.vs.californium.coap.Response;
 import ch.ethz.inf.vs.californium.network.CoAPEndpoint;
-import ch.ethz.inf.vs.californium.network.Exchange;
 import ch.ethz.inf.vs.californium.network.config.NetworkConfig;
 import ch.ethz.inf.vs.californium.server.Server;
+import ch.ethz.inf.vs.californium.server.resources.CoapExchange;
 import ch.ethz.inf.vs.californium.server.resources.ResourceBase;
 import ch.ethz.inf.vs.scandium.DTLSConnector;
 
 public class SecureServer {
 	
-	public static final int DTLS_PORT = 5685; // TBD
+	private static final Logger Log = CalifonriumLogger.getLogger(SecureServer.class);
+	
+	public static final int DTLS_PORT = 5684;
 
 	public static void main(String[] args) {
+		
 		System.out.println("Start secure server and bind to port "+DTLS_PORT);
-		InetSocketAddress address = new InetSocketAddress(DTLS_PORT);
 		
 		Server server = new Server();
-		server.add(new ResourceBase("hello") {
-			
-			@Override
-			public void handleGET(Exchange exchange) {
-				Response response = new Response(ResponseCode.CONTENT);
-				response.setPayload("hello security");
-				respond(exchange, response);
-			}
-		});
-		server.addEndpoint(new CoAPEndpoint(new DTLSConnector(address), NetworkConfig.getStandard()));
+		server.add(new ResourceBase("secure") {	
+				@Override
+				public void handleGET(CoapExchange exchange) {
+					exchange.respond(ResponseCode.CONTENT, "hello security");
+				}
+			});
+        server.addEndpoint(new CoAPEndpoint(new DTLSConnector(new InetSocketAddress(DTLS_PORT)), NetworkConfig.getStandard()));
 		server.start();
 		
 		System.out.println("Secure CoAP server powered by Scandium (Sc) is listening on port "+DTLS_PORT);
+		
+		Log.setLevel(Level.INFO);
 	}
 
 }
