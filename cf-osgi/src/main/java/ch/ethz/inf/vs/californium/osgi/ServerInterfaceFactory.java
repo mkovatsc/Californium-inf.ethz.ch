5941508c3c1e49a58ad868c57d12a8fe8b323ab2
Kai Hudalla <kai.hudalla@bosch-si.com>
2013-12-17 15:11:01 +0100

    Use standard property DEFAULT_COAP_PORT for configuring CoAP endpoint port, better use Californium Server's constructors in order to avoid generation of default endpoint if not necessary, add ConnectorFactory to OSGi bundle in order to support definition of a secure endpoint without a direct dependency on Scandium

--- cf-osgi/src/main/java/ch/ethz/inf/vs/californium/osgi/ServerInterfaceFactory.java
+++ cf-osgi/src/main/java/ch/ethz/inf/vs/californium/osgi/ServerInterfaceFactory.java
@@ -1,5 +1,6 @@
 package ch.ethz.inf.vs.californium.osgi;
 
+import ch.ethz.inf.vs.californium.network.config.NetworkConfig;
 import ch.ethz.inf.vs.californium.server.Server;
 import ch.ethz.inf.vs.californium.server.ServerInterface;
 
@@ -14,10 +15,26 @@ interface ServerInterfaceFactory {
 	
 	/**
 	 * Creates a new {@link ServerInterface} instance.
+	 * 
 	 * Can be overridden e.g. by test classes to use a mock instance instead of a <i>real</i> server.
 	 * This default implementation returns a new instance of {@link Server}.
 	 * 
+	 * @param config the network configuration to use for setting up the server's endpoint. If <code>null</code>
+	 * the default network configuration is used.
 	 * @return the new instance
 	 */
-	ServerInterface newServer();
+	ServerInterface newServer(NetworkConfig config);
+	
+	/**
+	 * Creates a new {@link ServerInterface} instance with multiple endpoints.
+	 * 
+	 * Can be overridden e.g. by test classes to use a mock instance instead of a <i>real</i> server.
+	 * This default implementation returns a new instance of {@link Server}.
+	 * 
+	 * @param config the network configuration to use for setting up the server's endpoints. If <code>null</code>
+	 * the default network configuration is used.
+	 * @param ports the ports to bind endpoints to
+	 * @return the new instance
+	 */
+	ServerInterface newServer(NetworkConfig config, int... ports);
 }
