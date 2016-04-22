ade2681a36752bda0da2961e77c14e1aa0bfdc86
Matthias Kovatsch <kovatsch@inf.ethz.ch>
2016-04-22 16:38:17 +0200

    Removed deprecated content

--- californium-proxy/src/main/java/ch/ethz/inf/vs/californium/proxy/RequestHandler.java
+++
@@ -1,9 +0,0 @@
-package ch.ethz.inf.vs.californium.proxy;
-
-import ch.ethz.inf.vs.californium.coap.Request;
-
-public interface RequestHandler {
-
-	public void handleRequest(Request request);
-	
-}
